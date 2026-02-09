7ffa40198265f3438de767b929ca337f9345917f
Owen Johnson <oij13@icloud.com>
2026-02-09 11:55:38 -0500

    Create graphql project

---
+++ graphqlactivity/graphql/resolver.ts
@@ -0,0 +1,22 @@
+import {db} from "./data";
+
+interface WeatherInterface {
+    zip: string;
+    weather: string;
+    tempC: string;
+    tempF: string;
+    friends: string[]
+}
+
+export const resolvers = {
+    Query: {
+        weather: async (_: unknown, param: WeatherInterface) => {
+            return [db.find(item => item.zip === param.zip)];
+        }
+    },
+    Mutation: {
+        weather: async (_: unknown, param: { data: WeatherInterface }) => {
+            return [db.find(item => item.zip === param.data.zip)];
+        }
+    }
+}
